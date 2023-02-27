2139a219c084932b1f599f53b19780bc659db61b
Pmpeat <36558975+Pmpeat@users.noreply.github.com>
2023-02-27 15:54:44 +0700

    Update DialogFlow.controller.js

--- src/controllers/v1/DialogFlow.controller.js
+++ src/controllers/v1/DialogFlow.controller.js
@@ -273,9 +273,95 @@ class DialogFlow extends BaseController {
                       }
                       dataToMsg.push(arrayData);
                  });
-                 texts = {
+                //  texts = {
+                //     "type": "carousel",
+                //     "contents": [...dataToMsg]
+                //   }
+
+                texts = {
                     "type": "carousel",
-                    "contents": [...dataToMsg]
+                    "contents": [
+                  
+                      {
+                        "type": "bubble",
+                        "size": "nano",
+                        "header": {
+                          "type": "box",
+                          "layout": "vertical",
+                          "contents": [
+                            {
+                              "type": "text",
+                              "text": "In Progress",
+                              "color": "#ffffff",
+                              "align": "start",
+                              "size": "md",
+                              "gravity": "center"
+                            },
+                            {
+                              "type": "text",
+                              "text": "100%",
+                              "color": "#ffffff",
+                              "align": "start",
+                              "size": "xs",
+                              "gravity": "center",
+                              "margin": "lg"
+                            },
+                            {
+                              "type": "box",
+                              "layout": "vertical",
+                              "contents": [
+                                {
+                                  "type": "box",
+                                  "layout": "vertical",
+                                  "contents": [
+                                    {
+                                      "type": "filler"
+                                    }
+                                  ],
+                                  "width": "100%",
+                                  "backgroundColor": "#7D51E4",
+                                  "height": "6px"
+                                }
+                              ],
+                              "backgroundColor": "#9FD8E36E",
+                              "height": "6px",
+                              "margin": "sm"
+                            }
+                          ],
+                          "backgroundColor": "#A17DF5",
+                          "paddingTop": "19px",
+                          "paddingAll": "12px",
+                          "paddingBottom": "16px"
+                        },
+                        "body": {
+                          "type": "box",
+                          "layout": "vertical",
+                          "contents": [
+                            {
+                              "type": "box",
+                              "layout": "horizontal",
+                              "contents": [
+                                {
+                                  "type": "text",
+                                  "text": "Buy milk and lettuce before class",
+                                  "color": "#8C8C8C",
+                                  "size": "sm",
+                                  "wrap": true
+                                }
+                              ],
+                              "flex": 1
+                            }
+                          ],
+                          "spacing": "md",
+                          "paddingAll": "12px"
+                        },
+                        "styles": {
+                          "footer": {
+                            "separator": false
+                          }
+                        }
+                      }
+                    ]
                   }
 
                  if(!_.isNull(result)){
