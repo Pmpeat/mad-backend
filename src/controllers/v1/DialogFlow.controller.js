0a249b41aded774f3cc3b39881eeadb5a06ce41c
Pmpeat <36558975+Pmpeat@users.noreply.github.com>
2023-02-27 16:20:58 +0700

    Update DialogFlow.controller.js

--- src/controllers/v1/DialogFlow.controller.js
+++ src/controllers/v1/DialogFlow.controller.js
@@ -268,7 +268,7 @@ class DialogFlow extends BaseController {
                   }
 
                  if(!_.isNull(result)){
-                    return client.replyMessage(event.replyToken,texts);
+                    return client.pushMessage(event.replyToken,texts);
                 } else {
                     return client.replyMessage(event.replyToken,{type:'text',text:"ระบบขัดข้อง กรุณาติดต่อฝ่าย it"});
                 }
