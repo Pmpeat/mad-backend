f2090918301a92a6658bf5d797cd9148115beeec
Pmpeat <36558975+Pmpeat@users.noreply.github.com>
2023-02-27 16:17:27 +0700

    Update DialogFlow.controller.js

--- src/controllers/v1/DialogFlow.controller.js
+++ src/controllers/v1/DialogFlow.controller.js
@@ -258,14 +258,86 @@ class DialogFlow extends BaseController {
                         "type": "box",
                         "layout": "vertical",
                         "contents": [
-                          {
-                            "type": "text",
-                            "text": "hello"
-                          },
-                          {
-                            "type": "text",
-                            "text": "world"
-                          }
+                            {
+                                "type": "bubble",
+                                "header": {
+                                  "type": "box",
+                                  "layout": "vertical",
+                                  "contents": [
+                                    {
+                                      "type": "text",
+                                      "text": "Header text"
+                                    }
+                                  ]
+                                },
+                                "hero": {
+                                  "type": "image",
+                                  "url": "https://example.com/flex/images/image.jpg"
+                                },
+                                "body": {
+                                  "type": "box",
+                                  "layout": "vertical",
+                                  "contents": [
+                                    {
+                                      "type": "text",
+                                      "text": "Body text"
+                                    }
+                                  ]
+                                },
+                                "footer": {
+                                  "type": "box",
+                                  "layout": "vertical",
+                                  "contents": [
+                                    {
+                                      "type": "text",
+                                      "text": "Footer text"
+                                    }
+                                  ]
+                                },
+                                "styles": {
+                                  "comment": "See the example of a bubble style object"
+                                }
+                              },
+                              {
+                                "type": "bubble",
+                                "header": {
+                                  "type": "box",
+                                  "layout": "vertical",
+                                  "contents": [
+                                    {
+                                      "type": "text",
+                                      "text": "Header text"
+                                    }
+                                  ]
+                                },
+                                "hero": {
+                                  "type": "image",
+                                  "url": "https://example.com/flex/images/image.jpg"
+                                },
+                                "body": {
+                                  "type": "box",
+                                  "layout": "vertical",
+                                  "contents": [
+                                    {
+                                      "type": "text",
+                                      "text": "Body text"
+                                    }
+                                  ]
+                                },
+                                "footer": {
+                                  "type": "box",
+                                  "layout": "vertical",
+                                  "contents": [
+                                    {
+                                      "type": "text",
+                                      "text": "Footer text"
+                                    }
+                                  ]
+                                },
+                                "styles": {
+                                  "comment": "See the example of a bubble style object"
+                                }
+                              }
                         ]
                       }
                     }
